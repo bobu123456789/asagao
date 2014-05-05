2df8c4d981bbc8e2c13e1295fd7569942f7b1b2f
fbsuser1 <fbsuser1@fbsuser1s-MacBook-Pro.local>
2014-05-05 14:25:52 +0900

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asagao::Application.config.secret_token = '9ed69ee9c129fef2e7e153776af9b1dbcfddaf65b1963a6c5be2e70697c95b336c32c1f50b439663466ff47eaeb3542869efa83424c459dac844eef42d0a3755'
